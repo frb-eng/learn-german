2ba30e09b6b102a4fa561fb8c1702db7a5e6b202
Mykolas <maiklasm@gmail.com>
2024-07-03 22:50:53 +0200

    improve environment setup instructions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-# Required for python
-# sudo apt-get update && sudo apt-get install build-essential sqlite3
-
-# Install homebrew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Make sure you have the following in .bashrc
-# alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-# eval "$(pyenv init -)"
-# eval "$(pyenv virtualenv-init -)"
-
 project=learn-german
 python_version=3.12.3
-
-brew update && brew install python-tk pyenv pyenv-virtualenv 
 
 if ! pyenv virtualenvs | grep -q "$project"; then
     pyenv install $python_version
